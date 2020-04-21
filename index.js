a9e0970099c56e8fa6436f847843292c142dcacb
HelenHMaher <helenharlanmaher@gmail.com>
2020-04-21 17:50:03 +0200

    convert state abbrev

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
+//states abbrev. to state names
+
+const states = {
+  AL: "Alabama",
+  AK: "Alaska",
+  AZ: "Arizona",
+  AR: "Arkansas",
+  CA: "California",
+  CO: "Colorado",
+  CT: "Connecticut",
+  DE: "Delaware",
+  DC: "District Of Columbia",
+  FL: "Florida",
+  GA: "Georgia",
+  HI: "Hawaii",
+  ID: "Idaho",
+  IL: "Illinois",
+  IN: "Indiana",
+  IA: "Iowa",
+  KS: "Kansas",
+  KY: "Kentucky",
+  LA: "Louisiana",
+  ME: "Maine",
+  MD: "Maryland",
+  MA: "Massachusetts",
+  MI: "Michigan",
+  MN: "Minnesota",
+  MS: "Mississippi",
+  MO: "Missouri",
+  MT: "Montana",
+  NE: "Nebraska",
+  NV: "Nevada",
+  NH: "New Hampshire",
+  NJ: "New Jersey",
+  NM: "New Mexico",
+  NY: "New York",
+  NC: "North Carolina",
+  ND: "North Dakota",
+  OH: "Ohio",
+  OK: "Oklahoma",
+  OR: "Oregon",
+  PA: "Pennsylvania",
+  RI: "Rhode Island",
+  SC: "South Carolina",
+  SD: "South Dakota",
+  TN: "Tennessee",
+  TX: "Texas",
+  UT: "Utah",
+  VT: "Vermont",
+  VA: "Virginia",
+  WA: "Washington",
+  WV: "West Virginia",
+  WI: "Wisconsin",
+  WY: "Wyoming",
+};
+
 //define svg area
 
 const width = 1000,
@@ -18,7 +74,7 @@ const tooltip = svgContainer
   .attr("id", "tooltip")
   .style("opacity", 0);
 
-//import data
+//import data including json to converst state abbreviation to name
 
 const COUNTIES_FILE =
   "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
@@ -49,9 +105,50 @@ d3.queue()
 
     const legend = svgContainer.append("g").attr("id", "legend");
 
-    //visualize counties
+    //constants used to visualize counties with colors and tooltip
 
-    geoGenerator = d3.geoPath();
+    const geoGenerator = d3.geoPath();
+    const getCountyName = (d) => {
+      let countyName = educationFile.filter((x) => {
+        return x.fips === d.id;
+      });
+      if (countyName[0]) {
+        return countyName[0].area_name;
+      } else {
+        console.log("error at: " + d.id);
+        return "--";
+      }
+    };
+    const getStateName = (d) => {
+      let stateName = educationFile.filter((x) => {
+        return x.fips === d.id;
+      });
+      if (stateName[0]) {
+        return states[stateName[0].state];
+      } else {
+        console.log("error at: " + d.id);
+        return "--";
+      }
+    };
+    const getEducationData = (d) => {
+      let education = educationFile.filter((x) => {
+        return x.fips === d.id;
+      });
+      if (education[0]) {
+        return education[0].bachelorsOrHigher;
+      } else {
+        console.log("error at: " + d.id);
+        return 0;
+      }
+    };
+    const textContent = (d) => {
+      getCountyName(d) +
+        "<br>" +
+        getStateName(d) +
+        "<br>" +
+        getEducationData(d);
+    };
+    //visualize counties
 
     svgContainer
       .append("g")
@@ -63,25 +160,25 @@ d3.queue()
       .attr("class", "county")
       .attr("data-fips", (d) => d.id)
       .attr("data-education", (d) => {
-        let education = educationFile.filter((x) => {
-          return x.fips === d.id;
-        });
-        if (education[0]) {
-          return education[0].bachelorsOrHigher;
-        } else {
-          console.log("error at: " + d.id);
-          return 0;
-        }
+        return getEducationData(d);
       })
       .attr("fill", (d) => {
-        let education = educationFile.filter((x) => {
-          return x.fips === d.id;
-        });
-        if (education[0]) {
-          return "#" + colorScale(education[0].bachelorsOrHigher) + "00000";
-        } else {
-          return "#" + colorScale(0) + "00000";
-        }
+        return "#" + colorScale(getEducationData(d)) + "00000";
       })
+      .attr("d", geoGenerator)
+      .on("mouseover", (d, i) => {
+        tooltip
+          .style("opacity", 0.8)
+          .attr("data-education", getEducationData(d))
+          .html(textContent(d));
+      });
+
+    //visualize states
+
+    svgContainer
+      .append("path")
+      .datum(topojson.mesh(usFile, usFile.objects.states, (a, b) => a !== b))
+      .attr("fill", "none")
+      .attr("class", "state")
       .attr("d", geoGenerator);
   });
